0ce680785822f55da88b7a61d944d5f8c02398e2
jusvait <42100008+jusvait@users.noreply.github.com>
2021-01-11 12:30:09 +0200

    Initialize project

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const WolframAlphaAPI = require('wolfram-alpha-api');
+const waApi = WolframAlphaAPI('APPID');
+
+
+// Discord bot
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.channel.send('pong');
+    }
+});
+
+client.login('token');
